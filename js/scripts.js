52723d51141b9b66b47fc487863f2feeb9db4cf4
Emerson Jordan and Rochelle Roberts <rochelle@>
2019-06-17 12:00:36 -0700

    did cool things

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,41 @@
+$(function(){
+
+for(i = 1; i < 11; i++){
+var mexico = new Destination("Tijuana", "la playa", "tiempos buenos");
+var book = new DestinationBook("trip--" + i);
+DestinationBookManager(book);
+book.addDestination(mexico);
+book.addDestination(new Destination("Colorado", "mountain", "winter"));
+}
+//
+// var mexico = new Destination("Tijuana", "la playa", "tiempos buenos");
+// var book = new DestinationBook("trips" + 2);
+// DestinationBookManager(book);
+// book.addDestination(mexico);
+// book.addDestination(new Destination("Colorado", "mountain", "winter"));
+
+// book.deleteDestination(5);
+// book.deleteDestination(0);
+
+});
+
+function DestinationBookManager(dBook){
+  $(".destinationList").after("<ul id = " + dBook.name + ">" + dBook.name + "</ul>");
+  $("#" + dBook.name).click(function(event){
+      event.preventDefault();
+    // $("#" + dBook.name + " li").each(function(d){
+      $(".d" + dBook.name).toggle();
+    // });
+  });
+}
+
+function DestinationManager(dBook, destination){
+  $("#" + dBook.name).append("<li class = d" + dBook.name + ">"+destination.location+"</li>");
+  $(".d" + dBook.name).hide();
+}
+
+
+
 function Destination(location, landmark, time){
   this.location = location,
   this.landmark = landmark,
@@ -18,6 +56,8 @@ DestinationBook.prototype.addDestination = function(destination) {
     destination.id = this.idCounter;
     this.idCounter++;
     this.list.push(destination);
+    //should be in front end code only
+    DestinationManager(this, destination);
 }
 
   DestinationBook.prototype.deleteDestination = function(id) {
@@ -36,17 +76,3 @@ DestinationBook.prototype.printDestinations = function(){
     d.logInfo();
   })
 }
-
-
-
-var mexico = new Destination("Tijuana", "la playa", "tiempos buenos");
-var book = new DestinationBook("trips");
-
-book.addDestination(mexico);
-book.addDestination(new Destination("Colorado", "mountain", "winter"));
-
-book.printDestinations();
-
-book.deleteDestination(5);
-book.deleteDestination(0);
-book.printDestinations();
